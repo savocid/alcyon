09d46a66dc30bd713f44a787ac2a3f2e6c66c0e3
alumen <fixiiexd@gmail.com>
2025-02-22 10:44:06 +0100

    alcyon config setup / a_partymon_fieldmove bool

---
+++ include/config/alcyon.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_CONFIG_ALCYON_H
+#define GUARD_CONFIG_ALCYON_H
+
+// Alcyon config
+#define A_PARTYMON_FIELDMOVE   FALSE           // If TRUE, the player's Pokémon can use field moves from party menu.
+
+
+#endif // GUARD_CONFIG_ALCYON_H
