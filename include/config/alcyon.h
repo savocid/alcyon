963db07fe3d76bc2ef9e1b2f38166254f7932116
alumen <fixiiexd@gmail.com>
2025-03-03 21:49:31 +0100

    config for always obey / hidden ability from wild or gift pokemon / move tutor egg moves / move tutor egg moves minimum level of pokemon

--- include/config/alcyon.h
+++ include/config/alcyon.h
@@ -2,7 +2,11 @@
 #define GUARD_CONFIG_ALCYON_H
 
 // Alcyon config
-#define A_PARTYMON_FIELDMOVE   FALSE           // If TRUE, the player's Pokémon can use field moves from party menu.
-#define A_PERMADEATH           TRUE            // If TRUE, the player's Pokémon cannot be healed when fainted (Whiteout will still heal the player's party).
+#define A_PARTYMON_FIELDMOVE        FALSE    // If TRUE, the player's Pokémon can use field moves from party menu.
+#define A_PERMADEATH                TRUE     // If TRUE, the player's Pokémon cannot be healed when fainted (Whiteout will still heal the player's party).
+#define A_ALWAYS_OBEY               TRUE     // If TRUE, the player's Pokémon will always obey regardless of level/badge.
+#define A_MON_MAY_HIDDEN_ABILITY    TRUE     // If TRUE, Wild Encounter / Gift Pokémon may have their hidden abilities
+#define A_TUTOR_EGG_MOVES           TRUE     // If TRUE, egg moves can be taught by move tutors
+#define A_TUTOR_EGG_MOVES_MIN_LEVEL 30       // Minimum level required to learn egg moves by move tutor
 
 #endif // GUARD_CONFIG_ALCYON_H
